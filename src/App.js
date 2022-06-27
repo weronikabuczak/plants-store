d9a4c114d877078ada667bc73bcc5f377fa33752
weronikakurczyna <56171356+weronikakurczyna@users.noreply.github.com>
2022-06-27 13:34:51 +0200

    Add footer

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import NavigationBar from "./components/NavigationBar/NavigationBar";
 import {Fragment} from 'react';
 import ProductsList from "./components/ProductsList/ProductsList";
+import Footer from "./components/Footer/Footer";
 
 function App() {
     return (
         <Fragment>
             <NavigationBar/>
             <ProductsList/>
+            <Footer/>
         </Fragment>
-    //    todo footer
+
     );
 }
 
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,12 @@
+import classes from './Footer.module.css'
+
+const Footer = () => {
+    return(
+        <footer className={classes.footer}>
+            <h2>House Plants Store 2022</h2>
+            <p>Other information</p>
+        </footer>
+    )
+}
+
+export default Footer;
